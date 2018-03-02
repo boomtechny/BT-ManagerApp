60552dc6b1c345c7a03eb00b3f713d5a618c41b1
BoomTech <boomtechny@gmail.com>
2018-03-02 02:04:18 -0500

    Configured firebase. Created constants for api keys and added file to gitignore. Added common components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
-
 import React, {Component} from 'react'; 
 import {View, Text} from 'react-native'; 
+import * as firebase from 'firebase';
 import { Provider } from 'react-redux'; 
 import { createStore } from 'redux'; 
 import reducers from './reducers'; 
+import  { FB_APIKEY, FB_AUTHDOMAIN, FB_DATABASEURL, FB_PROJECTID, FB_STORAGEBUCKET, FB_MESSAGINGSENDERID} from '../config/constants.js';
 class App extends Component{
+  componentWillMount(){
+  var app = firebase.initializeApp({
+    apiKey: FB_APIKEY,
+    authDomain: FB_AUTHDOMAIN,
+    databaseURL: FB_DATABASEURL,
+    projectId: FB_PROJECTID,
+    storageBucket: FB_STORAGEBUCKET,
+    messagingSenderId: FB_MESSAGINGSENDERID
+  });
+  }
   render(){
     return(  <Provider store = {createStore(reducers)}>
     <View>
