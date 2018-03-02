dd790afd91787457c827a5e52b00552ecc6f3cc0
BoomTech <boomtechny@gmail.com>
2018-03-02 01:47:36 -0500

    Intial configuation of root component as class componnet and redux integration

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 
-AppRegistry.registerComponent('btreactnativereduxapp', () => App);
+AppRegistry.registerComponent('manager', () => App);
