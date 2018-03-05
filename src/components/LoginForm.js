e770d4d7730be4cd9763b179683f5f70993dbfe3
BoomTech <boomtechny@gmail.com>
2018-03-05 17:29:03 -0500

    Added login user response helper functions and added case for failed login user, added new piece of state to reducer, error, which will update if error is returned.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react'; 
+import { View, Text } from 'react-native'; 
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions'; 
 import { Card, CardSection, Input, Button } from './common'; 
@@ -21,6 +22,18 @@ this.props.emailChanged(text)
     
   }
 
+  renderError(){
+    if(this.props.error){
+      return(
+        <View style = {{backgroundColor: 'white'}}>
+        <Text style = {styles.errorTextStyle}>
+        {this.props.error}
+        </Text>
+        </View>
+      );
+    }
+  }
+
   render(){
   return ( 
   <Card>
@@ -31,6 +44,7 @@ this.props.emailChanged(text)
       <CardSection>
       <Input secureTextEntry label = "Password" placeholder = "password" onChangeText = {this.onPasswordChange.bind(this)} value ={this.props.password} />
       </CardSection>
+      {this.renderError()}
       <CardSection>
       <Button onPress={this.onButtonPress.bind(this)}> Login </Button> 
       </CardSection>
@@ -39,10 +53,19 @@ this.props.emailChanged(text)
   }
 }
 
+const styles = {
+  errorTextStyle: {
+    fontSize: 20, 
+    alignSelf: 'center', 
+    color: 'red'
+  }
+}
+
 const mapStateToProps = state => {
   return{
     email: state.auth.email, 
-    password: state.auth.password
+    password: state.auth.password, 
+    error: state.auth.error
   };
 };
 
