64f79ce198d2d6798b6c3811123b32fed772482a
BoomTech <boomtechny@gmail.com>
2018-03-06 14:08:19 -0500

    Dynamic datasource building utilizing lifecycle methods.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux';
-import {View, Text } from 'react-native'; 
+import {View, Text, ListView } from 'react-native'; 
 import { employeesFetch } from '../actions'
 
 class EmployeeList extends Component {
   
   componentWillMount(){
     this.props.employeesFetch();
+    this.createDataSoure(this.props);
   }
+
+  componentWillReceiveProps(nextProps){
+
+this.createDataSoure(nextProps);
+
+  }
+  
+  createDataSoure({employees}){
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1,r2) => r1 !== r2 }); 
+    }); 
+    this.dataSource = ds.cloneWithRows(this.props.employees); 
+  }
+  
   render(){
     return (
       <View>
