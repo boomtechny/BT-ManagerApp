56485a1f9e2eb5105c1185c56901e632de5c4708
BoomTech <boomtechny@gmail.com>
2018-03-06 17:32:56 -0500

    Configured EmployeeList with ListView to populate with employee list data and created ListItem component to render each employee name on a row. Note: ListView has been deprecated so will have to follow up with replacing with FlatList or SectionList.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,8 +1,9 @@
 import _ from 'lodash';
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux';
-import {View, Text, ListView } from 'react-native'; 
-import { employeesFetch } from '../actions'
+import { ListView } from 'react-native'; 
+import { employeesFetch } from '../actions';
+import ListItem from './ListItem'; 
 
 class EmployeeList extends Component {
   
@@ -21,21 +22,17 @@ this.createDataSoure(nextProps);
     const ds = new ListView.DataSource({
       rowHasChanged: (r1,r2) => r1 !== r2 }); 
    
-    this.dataSource = ds.cloneWithRows(this.props.employees); 
+    this.dataSource = ds.cloneWithRows(employees); 
   }
 
+  renderRow(employee){
+    return <ListItem employee={employee}/>;
+  }
   render(){
     console.log(this.props); 
     return (
-      <View>
-        <Text> Employee List</Text>
-        <Text> Employee List</Text>
-        <Text> Employee List</Text>
-        <Text> Employee List</Text>
-        <Text> Employee List</Text>
-        <Text> Employee List</Text>
-        <Text> Employee List</Text>
-        </View>
+  <ListView enableEmptySections dataSource={this.dataSource} renderRow={this.renderRow}
+  />
     );
   }
 }
