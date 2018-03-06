5c569fb194160e45c92f69ff99cf62b9e49683c0
BoomTech <boomtechny@gmail.com>
2018-03-06 04:41:46 -0500

    Created action for employeeactions and reducer.

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,11 @@
+import { EMPLOYEE_UPDATE } from '../actions/./types'; 
+
+const INTIAL_STATE = {}; 
+
+export default (state = INITIAL_STATE, action) =>{
+  switch(action.type){
+    default:
+    return state; 
+
+  }
+}; 
