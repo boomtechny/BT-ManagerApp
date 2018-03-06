a504cb2166b710c9d464ad4332f50caeca5e06da
BoomTech <boomtechny@gmail.com>
2018-03-06 05:31:26 -0500

    Added Picker Component to EmployeeCreate Form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'; 
+import { Picker } from 'react-native'; 
 import { connect } from 'react-redux'; 
 import { employeeUpdate } from '../actions'
 import {Card, CardSection , Input, Button} from './common'; 
@@ -15,7 +16,19 @@ class EmployeeCreate extends Component {
      <CardSection>
        <Input label = "Phone" placeholder = "555-555-5555" value={this.props.phone} onChangeText={value=> this.props.employeeUpdate({prop: 'phone', value})}/>
        </CardSection>
-     </Card>
+  
+      <CardSection>
+     <Picker style = {{flex:1}} selectedValue={this.props.shift} onValueChange = {value => this.props.employeeUpdate({prop: 'shift', value})}>
+       <Picker.Item label="Monday" value="Monday"/>
+       <Picker.Item label="Tuesday" value="Tuesday"/>
+       <Picker.Item label="Wednesday" value="Wednesday"/>
+       <Picker.Item label="Thursday" value="Thursday"/>
+       <Picker.Item label="Friday" value="Friday"/>
+       <Picker.Item label="Saturday" value="Saturday"/>
+       <Picker.Item label="Sunday" value="Sunday"/>
+       </Picker>
+      </CardSection>
+    </Card>
     );
   }
 }
