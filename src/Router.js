1d5478afad5b862e11fb4d21b3585dd4854bbc6d
BoomTech <boomtechny@gmail.com>
2018-03-06 04:21:57 -0500

    Removed back button from navigation from EmployeeList Screen. V4 of RNRF utilizes ActionConst api to manage back

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,20 @@
 import React from 'react'; 
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, ActionConst } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm'; 
-import EmployeeList from './components/EmployeeList'
+import EmployeeList from './components/EmployeeList'; 
+
 const RouterComponent = () => {
 
   return(
     <Router >
-    <Scene key = "root" sceneStyle={{paddingTop: 65}}>
+   <Scene key = "root">
+    <Scene key = "auth" sceneStyle={{paddingTop: 65}}>
       <Scene key = "login" component={LoginForm} title = "Please Login" />
-      <Scene key = "employeeList" component={EmployeeList} title = "Employees" /> 
+      </Scene>
+     
+      <Scene key = "main" sceneStyle={{paddingTop: 65}} type={ActionConst.RESET}>
+      <Scene key = "employeeList" component={EmployeeList} title = "Employees" type={ActionConst.RESET}/> 
+      </Scene>
       </Scene>
       </Router>
   );
